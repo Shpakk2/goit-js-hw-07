27c8984223f8de672f9434976c4bb11ffb33d281
Andrii Shpak <SHPAKK2@GMAIL.COM>
2022-11-01 19:07:07 +0200

    firstTry

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery')
+
+galleryContainer.insertAdjacentHTML('beforeend', createGalleryItem(galleryItems))
+ 
+galleryContainer.addEventListener('click', openGalleryItem)
+
+function openGalleryItem (event) {
+    if (!event.target.classList.contains('gallery__image')) {
+    return
+    }
+    event.preventDefault()
+    const instance = basicLightbox.create(`<img width="100vw" height="100vh" src="${event.target.dataset.source}">`)
+        instance.show();
+
+    window.addEventListener('keydown', e => {
+        if (e.code === 'Escape') {
+        instance.close()
+        }
+    })
+}
+
+function createGalleryItem(galleryItems) { 
+    return galleryItems.map(({preview, original, description}) => {
+        return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </div>`
+    })
+    .join("")
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery')
+
+galleryContainer.insertAdjacentHTML('beforeend', createGalleryItem(galleryItems))
+
+function createGalleryItem(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<li class="gallery__item"><a  href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>`
+    }).join("")
+}
+
+let lightbox = new SimpleLightbox('.gallery a', {captionsData: "alt", captionPosition: "bottom", captionDelay: 250});
